44ce18bf418102d520dd3386b5940395b1d989ab
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2023-11-29 23:54:37 +0100

    Update push.sh

--- vars/push.sh
+++ vars/push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target 
-curl -X PUT -u 'admin:Admin321' -T *.jar http://localhost:8082/artifactory/example-repo-local/
+curl -X PUT -u 'admin:Admin321' -T *.jar http://localhost:8082/artifactory/java-web-app/
